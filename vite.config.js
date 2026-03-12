68e3531ab1e016136ea308945e916fa86fd53435
Mahaboob Shareef Shaik <shareefroshan12@gmail.com>
2026-03-11 21:36:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'ai-digital-stethoscope' with YOUR GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/ishareefshaik/ai-digital-stethoscope/',
+  base: '/ai-digital-stethoscope/',
 })
